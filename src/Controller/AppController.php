83ed4aa5df2bf46ac64fa4198b26ab87bfd219ca
Hasnaa Mohamed <hasnaa@mauve.de>
2023-08-18 15:52:46 +0300

    using twig

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
-class AppController
+class AppController extends AbstractController
 {
+    // #[Route('/', name: 'annotationRoute')] used in PHP 8
     /**
      * @Route("/",name="annotationRoute")
      * @return Response
@@ -15,6 +18,7 @@ class AppController
     public function index(){
         return new Response('Welcome to our first page',200);
     }
+    // #[Route('/', name: 'annotationRoute')] used in PHP 8
     /**
      * @Route("/wildCardRouteWithPost",name="wildCardRouteWithPost")
      * @return Response
@@ -22,20 +26,35 @@ class AppController
     public function id(Request $request){
         return new Response(sprintf('Welcome %s',$request->get('name')),200);
     }
-
+    // #[Route('/', name: 'annotationRoute')] used in PHP 8
     /**
-     * @Route("/wildCardRouteWithGet/{name}",name="wildCardRouteWithGet")
+     * @Route("/wildCardRouteWithGet/{name}/{category}",name="wildCardRouteWithGet")
      * @return Response
      */
-    public function name($name){
-        return new Response(sprintf('Welcome %s',$name),200);
-    }
+    public function name($name){//Must be the same name in route
+        $title=u(str_replace('_',' ',$name))->title(true);//make hasnaa_mohamed to Hasnaa Mohamed
 
+        return new Response(sprintf('Welcome %s',$title),200);
+    }
+    // #[Route('/', name: 'annotationRoute')] used in PHP 8
     /**
      * @Route("/renderTemplate/{name}",name="renderTemplate")
      * @return Response
      */
-    public function renderTemplate($name){
-        return new Response(sprintf('Welcome %s',$name),200);
+    public function renderTemplate($name=null){
+        if (empty($name)) {
+            dump("hasnaa");//test profiller
+            return $this->render('base.html.twig');
+        }
+        else
+            return new Response('Not Required',200);
+    }
+    /**
+     * @Route("/renderTemplateWithVariable",name="renderTemplate")
+     * @return Response
+     */
+    public function renderTemplateWithVariable(){
+        $users=['hasnaa','ahmed','ali'];
+            return $this->render('base.html.twig',['users' => $users]);
     }
 }
